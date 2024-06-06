f840f548d05cb074f001a5ef4bebc8a827a888d0
nar-deen <nardeenys@gmail.com>
2024-06-06 14:14:42 +0300

    nineth commit

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,10 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Frontpages;
 
-Route::get('/', function () {
+/*Route::get('/', function () {
     return view('test');
-});
+});*/
+
+Route::get('/', [Frontpages::class, 'home'])->name('home');
